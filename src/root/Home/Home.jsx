db30d5f33b9a66bfd8e584c74a20d88ebd1d776c
junaietfaruk <junaietfaruk@throughltd.com>
2026-02-05 23:24:05 +0600

    linkable footer

--- src/root/Home/Home.jsx
+++ src/root/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../component/Banner/Banner';
 import Doctors from '../../component/Doctors/Doctors';
 import Services from '../../component/Services/Services';
+import Footer from '../../component/Footer/Footer';
 
 const Home = () => {
     return (
@@ -9,6 +10,7 @@ const Home = () => {
            <Banner/>
            <Doctors/>
            <Services/>
+           <Footer/>
         </div>
     );
 };
